6f224e33d7479db288005b699130a695284430c5
photonnn <wasjiri@gmail.com>
2022-08-09 15:29:09 +0200

    Add project boilerplate

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: './dist',
+    open: {
+      app: {
+          name: 'google-chrome-stable',
+      },
+  },
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+  },
+};
